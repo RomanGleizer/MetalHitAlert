1a808c11061a8148af4c5a13f8e0ccbbe9a33687
lenchik007 <lena.ermal.kz@mail.ru>
2025-04-04 00:33:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 ﻿import os
 import numpy as np
-import soundfile as sf
-from scipy import signal
+import librosa
 import matplotlib.pyplot as plt
-import time
+audio_file_path = '0 тревога.mp3'
+audio_data, sample_rate = librosa.load(audio_file_path, sr=None)
+print("данные аудио:", audio_data)
+print("частота дискретизации:", sample_rate)
+plt.figure(figsize=(14, 5))
+librosa.display.waveshow(audio_data, sr=sample_rate)
+plt.title('аудиосигнал')
+plt.xlabel('время(с)')
+plt.ylabel('амплитуда:')
+plt.show()
+
+
+
+
